c6ac2559eea75acec0de750967f26f1ad4a47f97
kolya.cheran <nikolay.cheran@gmail.com>
2022-03-23 20:45:08 +0200

    iterator commit

---
+++ src/iterator/Iterator.java
@@ -0,0 +1,30 @@
+package iterator;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Iterator {
+
+    List<Integer> list = new LinkedList<>();
+    int currentIndex = 0;
+
+    public Iterator(int[][] arr) {
+        for (int [] intArr : arr){
+            for (int finalInt : intArr){
+                list.add(finalInt);
+            }
+        }
+        Collections.sort(list);
+    }
+
+    public boolean hasNext() {
+        return currentIndex < list.size();
+    }
+
+    public int next() {
+        int result = list.get(currentIndex);
+        currentIndex++;
+        return result;
+    }
+}
